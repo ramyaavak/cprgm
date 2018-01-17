c23c9ca9cd278adaebd6aa1a5b9080442efd338e
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-01-17 23:15:31 +1200

    greater.c
    greater

--- greater.c
+++ greater.c
@@ -4,14 +4,14 @@ int main()
 int a,b,c;
 printf("Enter a,b,c:");
 scanf("%d %d %d",&a,&b,&c);
-if((a>b)&&(a>c)){
-printf("\nais greater than b and c");
+if(a>b && a>c){
+printf("%d",a);
 }
-else if((b>a)&&(b>c)){
-printf("\nb is greater than a and c");
+else if(b>a && b>c){
+printf("%d",b);
 }
 else{
-printf("\nc is greater than a and b");
+printf("%d",c);
 }
 return 0;
 }
